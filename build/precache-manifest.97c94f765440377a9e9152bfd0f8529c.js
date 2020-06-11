2948c3cb5a3ceb21ed6ec6a10023c524c6f4f3f0
Jialiang Huang <jialianghuang@hotmail.com>
2020-06-10 18:17:10 -0700

    Add files via upload

---
+++ build/precache-manifest.97c94f765440377a9e9152bfd0f8529c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c67c64cb78d1ab43971d9f2e7194cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6759f641fcc8fa9da7ff",
+    "url": "./static/css/main.d09620a2.chunk.css"
+  },
+  {
+    "revision": "107ece76e547da6b4388",
+    "url": "./static/js/2.465cd0bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.465cd0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6759f641fcc8fa9da7ff",
+    "url": "./static/js/main.dbc992e7.chunk.js"
+  },
+  {
+    "revision": "84ade511e7dcd4ccd844",
+    "url": "./static/js/runtime-main.3d8a864f.js"
+  },
+  {
+    "revision": "03b10ca8c44d6347688b2a7db3c02420",
+    "url": "./static/media/product.03b10ca8.jpg"
+  }
+]);
